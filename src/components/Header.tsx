3fc087b87d3982793704d41db52e276767da461d
hyunwoo <pho9902@naver.com>
2023-02-28 03:32:00 +0900

    #4/woo: install react-router-dom

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,13 @@
 import styles from 'style/Header.module.scss';
+import {Link} from 'react-router-dom';
 
 const Header: React.FC = () => {
-  return <div className={styles.wrap}>헤더헤더</div>;
+  return (
+    <div className={styles.wrap}>
+      헤더헤더
+      <Link to="/mypage">로그인페이지</Link>
+    </div>
+  );
 };
 
 export default Header;
